3f1e063de958734f552ca09a9e9783f96da78d92
Dreagonmon <531486058@qq.com>
2025-07-16 12:39:57 +0800

    move remote logic to store

--- src/pages/controller/controller.tsx
+++ src/pages/controller/controller.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useRef, useState } from "preact/hooks";
+import { useMemo } from "preact/hooks";
 import { useStore } from "@nanostores/preact";
 import { useTranslator } from "../../hooks/i18n.ts";
 import { Icon } from "../../components/icon/icon.tsx";
@@ -6,43 +6,8 @@ import type { FunctionComponent } from "preact";
 import type { WaveDataGenerator } from "../../utils/coyote_ble.ts";
 
 import { $device, coyote } from "../../store/coyote.ts";
-
-interface SingleChannelRemoteConfig {
-    zapLevel?: number;
-    zapDurationMs?: number;
-    lowLevel?: number;
-    midLevel?: number;
-    highLevel?: number;
-}
-
-interface RemoteConfig {
-    a?: SingleChannelRemoteConfig;
-    b?: SingleChannelRemoteConfig;
-    entryName?: string;
-}
-
-const defaultRemoteConfig = () => {
-    return {
-        entryName: "",
-        a: {
-            zapLevel: 3,
-            zapDurationMs: 2000,
-            lowLevel: 1,
-            midLevel: 3,
-            highLevel: 5,
-        },
-        b: {
-            zapLevel: 0,
-            zapDurationMs: 0,
-            lowLevel: 0,
-            midLevel: 0,
-            highLevel: 0,
-        },
-    } as RemoteConfig;
-};
-const STATIC_REMOTE_CONFIG = defaultRemoteConfig();
-STATIC_REMOTE_CONFIG.entryName = localStorage.getItem("_entry_name_") ?? "";
-const STATIC_REMOTE_CONFIG_TEXT = JSON.stringify(STATIC_REMOTE_CONFIG, undefined, 2);
+import { $remoteConfigText, resetRemoteConfigText, setRemoteConfigText } from "../../store/coyote.ts";
+import { applyRemoteConfig, connectToEventSource } from "../../store/coyote.ts";
 
 const testWaveGenerator: WaveDataGenerator = {
     generateV3Wave: (_timeMs) => {
@@ -54,9 +19,7 @@ coyote.setWaveGeneratorA(testWaveGenerator);
 coyote.setWaveGeneratorB(testWaveGenerator);
 
 export const ControllerPage: FunctionComponent = (_) => {
-    const remoteConfig = useRef<RemoteConfig>(STATIC_REMOTE_CONFIG);
-    const eventSource = useRef<EventSource | undefined>(undefined);
-    const [configText, setConfigText] = useState(STATIC_REMOTE_CONFIG_TEXT);
+    const remoteConfigText = useStore($remoteConfigText);
     const device = useStore($device);
     const t = useTranslator();
     const toggleConnect = useMemo(() => async () => {
@@ -67,73 +30,26 @@ export const ControllerPage: FunctionComponent = (_) => {
             await coyote.connect();
         }
     }, [device]);
-    const applyRemoteConfig = useMemo(() => () => {
-        try {
-            const obj = JSON.parse(configText);
-            remoteConfig.current = obj as RemoteConfig;
+    const applyRemoteConfigWrapper = useMemo(() => () => {
+        if (applyRemoteConfig()) {
             return true;
-        } catch {
+        } else {
             alert(t({ zh: "JSON格式错误", en: "JSON Error" }));
-        }
-        return false;
-    }, [configText, remoteConfig, t]);
-    const connectRemoteServer = useMemo(() => () => {
-        if (!applyRemoteConfig()) return false;
-        if (eventSource.current) {
-            eventSource.current.close();
-        }
-        const entryName = remoteConfig.current?.entryName ?? "";
-        if (!entryName) {
-            alert(t({ zh: "entryName不能为空", en: "entryName can not be empty" }));
             return false;
         }
-        localStorage.setItem("_entry_name_", entryName);
-        const url = "/api/listen/" + entryName;
-        eventSource.current = new EventSource(url);
-        eventSource.current.addEventListener("message", (evt) => {
-            console.log("message:", evt.data);
-            const cfga: SingleChannelRemoteConfig = remoteConfig.current?.a ?? {};
-            const cfgb: SingleChannelRemoteConfig = remoteConfig.current?.b ?? {};
-            if (evt.data === "zap") {
-                coyote.setEnabledA((cfga.zapLevel ?? 0) > 0 ? true : false);
-                coyote.setLevelA(cfga.zapLevel ?? 0);
-                coyote.setEnabledB((cfgb.zapLevel ?? 0) > 0 ? true : false);
-                coyote.setLevelB(cfgb.zapLevel ?? 0);
-                setTimeout(() => {
-                    coyote.setLevelA(0);
-                    coyote.setEnabledA(false);
-                }, cfga.zapDurationMs ?? 0);
-                setTimeout(() => {
-                    coyote.setLevelB(0);
-                    coyote.setEnabledB(false);
-                }, cfgb.zapDurationMs ?? 0);
-            } else if (evt.data === "low") {
-                coyote.setEnabledA((cfga.lowLevel ?? 0) > 0 ? true : false);
-                coyote.setLevelA(cfga.lowLevel ?? 0);
-                coyote.setEnabledB((cfgb.lowLevel ?? 0) > 0 ? true : false);
-                coyote.setLevelB(cfgb.lowLevel ?? 0);
-            } else if (evt.data === "mid") {
-                coyote.setEnabledA((cfga.midLevel ?? 0) > 0 ? true : false);
-                coyote.setLevelA(cfga.midLevel ?? 0);
-                coyote.setEnabledB((cfgb.midLevel ?? 0) > 0 ? true : false);
-                coyote.setLevelB(cfgb.midLevel ?? 0);
-            } else if (evt.data === "high") {
-                coyote.setEnabledA((cfga.highLevel ?? 0) > 0 ? true : false);
-                coyote.setLevelA(cfga.highLevel ?? 0);
-                coyote.setEnabledB((cfgb.highLevel ?? 0) > 0 ? true : false);
-                coyote.setLevelB(cfgb.highLevel ?? 0);
-            } else if (evt.data === "off") {
-                coyote.setLevelA(0);
-                coyote.setEnabledA(false);
-                coyote.setLevelB(0);
-                coyote.setEnabledB(false);
-            }
-        });
-        eventSource.current.addEventListener("ping", (evt) => {
-            console.log("ping: ", evt.data);
-        });
-        return true;
-    }, [applyRemoteConfig, eventSource, remoteConfig, t]);
+    }, [applyRemoteConfig, t]);
+    const connectToEventSourceWrapper = useMemo(() => () => {
+        if (!applyRemoteConfig()) {
+            alert(t({ zh: "JSON格式错误", en: "JSON Error" }));
+            return false;
+        }
+        if (connectToEventSource()) {
+            return true;
+        } else {
+            alert(t({ zh: "entryName 不能为空", en: "entryName can not be empty" }));
+            return false;
+        }
+    }, [applyRemoteConfig, t]);
     const renderSimplePanel = useMemo(() =>
     (
         title: string,
@@ -203,16 +119,23 @@ export const ControllerPage: FunctionComponent = (_) => {
                 </div>
             </div>
             <div style={{ padding: "0 var(--pico-spacing)" }}>
-                <textarea rows={8} onInput={(evt) => setConfigText((evt.target as HTMLTextAreaElement).value ?? "")}>
-                    {configText}
+                <textarea
+                    rows={8}
+                    onInput={(evt) => setRemoteConfigText((evt.target as HTMLTextAreaElement).value ?? "")}
+                >
+                    {remoteConfigText}
                 </textarea>
             </div>
             <div style={{ width: "100%", padding: "0 var(--pico-spacing)" }}>
-                <button type="button" style={{ width: "100%" }} onClick={applyRemoteConfig}>
+                <button type="button" style={{ width: "100%" }} onClick={applyRemoteConfigWrapper}>
                     {t({ zh: "应用", en: "Apply" })}
                 </button>
-                <button type="button" style={{ width: "100%" }} onClick={connectRemoteServer}>
+                <button type="button" style={{ width: "100%" }} onClick={connectToEventSourceWrapper}>
                     {t({ zh: "连接远程服务器", en: "Connect to Remote" })}
+                </button>
+                <br/>
+                <button type="button" style={{ width: "100%" }} onClick={resetRemoteConfigText}>
+                    {t({ zh: "重置配置", en: "Reset Config" })}
                 </button>
             </div>
         </>
--- src/store/coyote.ts
+++ src/store/coyote.ts
@@ -1,4 +1,5 @@
 import { atom } from "nanostores";
+import { persistentAtom } from "@nanostores/persistent";
 import { CoyoteBLE } from "../utils/coyote_ble.ts";
 import type { CoyoteDeviceInfo } from "../utils/coyote_ble.ts";
 
@@ -15,3 +16,127 @@ coyote.addEventListener("change", (evt) => {
     const newStore: CoyoteDeviceStore = { ...evt.detail, coyote };
     $device.set(newStore);
 });
+
+/* ==== Remote Config ==== */
+export interface SingleChannelRemoteConfig {
+    zapLevel?: number;
+    zapDurationMs?: number;
+    lowLevel?: number;
+    midLevel?: number;
+    highLevel?: number;
+}
+
+export interface RemoteConfig {
+    a?: SingleChannelRemoteConfig;
+    b?: SingleChannelRemoteConfig;
+    entryName?: string;
+}
+
+export const makeDefaultRemoteConfig = () => {
+    return {
+        entryName: "",
+        a: {
+            zapLevel: 3,
+            zapDurationMs: 1000,
+            lowLevel: 1,
+            midLevel: 3,
+            highLevel: 5,
+        },
+        b: {
+            zapLevel: 0,
+            zapDurationMs: 0,
+            lowLevel: 0,
+            midLevel: 0,
+            highLevel: 0,
+        },
+    } as RemoteConfig;
+};
+
+export const $remoteConfigText = persistentAtom(
+    "coyote_remote_config",
+    JSON.stringify(makeDefaultRemoteConfig(), undefined, 2),
+);
+
+export const setRemoteConfigText = (newValue: string) => {
+    $remoteConfigText.set(newValue);
+};
+
+export const resetRemoteConfigText = () => {
+    $remoteConfigText.set(JSON.stringify(makeDefaultRemoteConfig()));
+};
+
+export const $remoteConfig = atom(makeDefaultRemoteConfig());
+
+export const applyRemoteConfig = () => {
+    try {
+        const obj = JSON.parse($remoteConfigText.get());
+        $remoteConfig.set(obj as RemoteConfig);
+        return true;
+    } catch {
+        return false;
+    }
+};
+
+/* ==== Remote Event Source ==== */
+
+export const $remoteEventSource = atom<EventSource | undefined>(undefined);
+
+export const connectToEventSource = () => {
+    if (!applyRemoteConfig()) return false;
+    const lastEventSource = $remoteEventSource.get();
+    if (lastEventSource) {
+        lastEventSource.close();
+    }
+    const entryName = $remoteConfig.get()?.entryName ?? "";
+    if (!entryName) {
+        return false;
+    }
+    localStorage.setItem("_entry_name_", entryName);
+    const url = "/api/listen/" + entryName;
+    const eventSource = new EventSource(url);
+    eventSource.addEventListener("message", (evt) => {
+        console.log("message:", evt.data);
+        const remoteConfig = $remoteConfig.get(); // get current config
+        const cfga: SingleChannelRemoteConfig = remoteConfig?.a ?? {};
+        const cfgb: SingleChannelRemoteConfig = remoteConfig?.b ?? {};
+        if (evt.data === "zap") {
+            coyote.setEnabledA((cfga.zapLevel ?? 0) > 0 ? true : false);
+            coyote.setLevelA(cfga.zapLevel ?? 0);
+            coyote.setEnabledB((cfgb.zapLevel ?? 0) > 0 ? true : false);
+            coyote.setLevelB(cfgb.zapLevel ?? 0);
+            setTimeout(() => {
+                coyote.setLevelA(0);
+                coyote.setEnabledA(false);
+            }, cfga.zapDurationMs ?? 0);
+            setTimeout(() => {
+                coyote.setLevelB(0);
+                coyote.setEnabledB(false);
+            }, cfgb.zapDurationMs ?? 0);
+        } else if (evt.data === "low") {
+            coyote.setEnabledA((cfga.lowLevel ?? 0) > 0 ? true : false);
+            coyote.setLevelA(cfga.lowLevel ?? 0);
+            coyote.setEnabledB((cfgb.lowLevel ?? 0) > 0 ? true : false);
+            coyote.setLevelB(cfgb.lowLevel ?? 0);
+        } else if (evt.data === "mid") {
+            coyote.setEnabledA((cfga.midLevel ?? 0) > 0 ? true : false);
+            coyote.setLevelA(cfga.midLevel ?? 0);
+            coyote.setEnabledB((cfgb.midLevel ?? 0) > 0 ? true : false);
+            coyote.setLevelB(cfgb.midLevel ?? 0);
+        } else if (evt.data === "high") {
+            coyote.setEnabledA((cfga.highLevel ?? 0) > 0 ? true : false);
+            coyote.setLevelA(cfga.highLevel ?? 0);
+            coyote.setEnabledB((cfgb.highLevel ?? 0) > 0 ? true : false);
+            coyote.setLevelB(cfgb.highLevel ?? 0);
+        } else if (evt.data === "off") {
+            coyote.setLevelA(0);
+            coyote.setEnabledA(false);
+            coyote.setLevelB(0);
+            coyote.setEnabledB(false);
+        }
+    });
+    eventSource.addEventListener("ping", (evt) => {
+        console.log("ping: ", evt.data);
+    });
+    $remoteEventSource.set(eventSource);
+    return true;
+};
