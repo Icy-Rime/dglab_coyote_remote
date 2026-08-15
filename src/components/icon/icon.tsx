53ff5c0c8f5d23290b3a5ab79f1d1feff114830e
Dreagonmon <531486058@qq.com>
2026-08-15 12:39:05 +0800

    improve loading sw

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,19 +1,45 @@
 import type { FunctionComponent, SVGAttributes } from "preact";
 import type { IconName } from "./feather_icon_name.d.ts";
-import { useMemo } from "preact/hooks";
+import { useMemo, useState } from "preact/hooks";
 import { useMergedClassName } from "../utils.ts";
 
 export type IconProps = SVGAttributes<SVGSVGElement> & {
     name: IconName;
 };
 
-const SVG_ICON_BASE_PATH = "svg/feather-sprite.svg#";
+const SVG_ICON_BASE_PATH = "svg/feather-sprite.svg";
+
+const cache = (() => {
+    const api = {
+        loaded: false,
+        blobUrl: "",
+        loadedCallback: [] as Array<() => void>,
+    };
+    (async () => {
+        const response = await fetch(SVG_ICON_BASE_PATH);
+        const blob = await response.blob();
+        api.blobUrl = URL.createObjectURL(blob);
+        api.loaded = true;
+        while (api.loadedCallback.length > 0) {
+            api.loadedCallback.shift()!();
+        }
+    })();
+    return api;
+})();
 
 export const Icon: FunctionComponent<IconProps> = (props: IconProps) => {
+    const [loaded, setLoaded] = useState(cache.loaded);
     const className = useMergedClassName("tinyui_svg_icon", props.class);
     const iconHref = useMemo(() => {
-        return SVG_ICON_BASE_PATH + props.name;
-    }, [props.name]);
+        if (loaded) {
+            return cache.blobUrl + "#" + props.name;
+        } else {
+            cache.loadedCallback.push(() => {
+                setLoaded(true);
+            });
+        }
+        return "";
+    }, [props.name, loaded]);
     return (
         <svg
             {...props}
--- src/sw.ts
+++ src/sw.ts
@@ -6,35 +6,47 @@
 const self: ServiceWorkerGlobalScope = globalThis.self as unknown as ServiceWorkerGlobalScope;
 
 import { registerRoute, Route } from "workbox-routing";
-import { CacheFirst } from "workbox-strategies";
+import { CacheFirst, NetworkOnly } from "workbox-strategies";
 import { ExpirationPlugin } from "workbox-expiration";
 
 let isDebuging = false;
 
-const cachedRoute = new Route(
-    ({ url, sameOrigin }) => {
-        if (isDebuging) {
+registerRoute(({ url }) => url.pathname.startsWith("/api/"), new NetworkOnly());
+registerRoute(
+    new Route(
+        ({ url, sameOrigin }) => {
+            if (isDebuging) {
+                return false;
+            }
+            const lowerPathname = url.pathname.toLowerCase();
+            if (lowerPathname.endsWith("feather-sprite.svg")) {
+                return true;
+            }
+            if (lowerPathname.endsWith("pico.min.css")) {
+                return true;
+            }
+            if (lowerPathname.endsWith("animate.min.css")) {
+                return true;
+            }
+            // console.log(url, sameOrigin);
+            // return sameOrigin && !(["127.0.0.1", "localhost"].includes(url.hostname));
+            // return sameOrigin && (!url.pathname.startsWith("/api/"));
             return false;
-        }
-        // console.log(url, sameOrigin);
-        // return sameOrigin && !(["127.0.0.1", "localhost"].includes(url.hostname));
-        // return sameOrigin && (!url.pathname.startsWith("/api/"));
-        return false;
-    },
-    new CacheFirst({
-        cacheName: "cached-route",
-        plugins: [
-            new ExpirationPlugin({
-                maxAgeSeconds: 24 * 60 * 60,
-            }),
-        ],
-    }),
+        },
+        new CacheFirst({
+            cacheName: "cached-route",
+            plugins: [
+                new ExpirationPlugin({
+                    maxAgeSeconds: 24 * 60 * 60,
+                }),
+            ],
+        }),
+    ),
 );
 
-registerRoute(cachedRoute);
-
 self.addEventListener("install", (event) => {
-    event.waitUntil(self.skipWaiting());
+    self.skipWaiting();
+    event.waitUntil(Promise.resolve());
 });
 self.addEventListener("activate", (event) => {
     event.waitUntil(self.clients.claim());
